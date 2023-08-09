6cef546c27e0fd701ee536780d8c5bcc544d44d0
Lucas Assis Pereira <53919220+LucasAssisPereira@users.noreply.github.com>
2023-08-09 10:46:10 -0300

    Adicionado atividade 1
    Primeira atividade da diciplina

---
+++ atividade1/verificaNumeroPrimo.js
@@ -0,0 +1,14 @@
+let num = 9;
+
+for (let index = 2; index < num; index++) {
+    if (num % index == 0) {
+        console.log(0);
+        break
+    }
+    else {
+        console.log(1); 
+        break
+    }
+}
+
+
